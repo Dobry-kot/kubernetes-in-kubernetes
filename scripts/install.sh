b96ee4ba8036838b897f11c9f361d17f042b1579
Dobry-kot <admin@dobry-kot.ru>
2022-03-26 19:12:50 +0100

    DK-11 Added the kube-apiserver chart and the correct etcd certificates to connect the api to etcd

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,25 +1,6 @@
 echo "Enter cluster name: "
 CLUSTER_NAME=cluster-2
 
-helm upgrade main charts/main/. -n $CLUSTER_NAME --install
-helm upgrade etcd charts/etcd/. -n $CLUSTER_NAME --install
-
-helm upgrade kube-apiserver-pre-proc charts/kube-apiserver-pre-proc/. -n $CLUSTER_NAME --install
-helm upgrade konnectivity-pre-proc charts/konnectivity-pre-proc/. -n $CLUSTER_NAME --install
-
-sleep 15
-
-LOADBALANCER_IP=$(kubectl get svc kubernetes-apiserver -n $CLUSTER_NAME -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-KONNECTIVITY_IP=$(kubectl get svc konnectivity-server -n $CLUSTER_NAME -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-
-echo $LOADBALANCER_IP
-echo $KONNECTIVITY_IP
-
-# 
-helm upgrade konnectivity charts/konnectivity/. -n $CLUSTER_NAME --install
-helm upgrade kube-apiserver charts/kube-apiserver/. -n $CLUSTER_NAME --install
-
-helm upgrade kube-controller-manager charts/kube-controller-manager/. -n $CLUSTER_NAME --install
-helm upgrade kube-scheduler charts/kube-scheduler/. -n $CLUSTER_NAME --install
-
-# helm del konnectivity-pre-proc kube-apiserver-pre-proc
+helm upgrade main charts/main/  --install --namespace=cluster-2 --wait
+helm upgrade konnectivity charts/konnectivity/
+helm install kube-apiserver charts/kube-apiserver/
